3e861c209190fe7a4081742e3423dee2dfc92bbc
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-01-18 14:33:50 +0330

    Add files via upload

---
+++ watch.py
@@ -0,0 +1,28 @@
+from telethon import TelegramClient, events, sync, functions, types
+
+api_id = 132654 # Change it
+api_hash = 'a91b87d20a000002bfc366b35ea121ae' # Change it
+
+bot = TelegramClient('watch.session', 181697, 'a91b87d20a2ab8f2bfc366b35ea121ae')
+
+@bot.on(events.NewMessage)
+async def handler(update):
+    deleted = False
+    if update.message.to_id.channel_id is not None:
+        if update.message.media is not None:
+            if update.message.media.__class__.__name__ == 'MessageMediaPoll':
+                user = await bot(functions.channels.GetParticipantRequest(channel=update.message.chat.id, user_id=update.message.from_id))
+                rights = user.participant.__class__.__name__
+                if rights != ' ChannelParticipantCreator' and rights != 'ChannelParticipantAdmin':
+                    await bot.delete_messages(update.message.chat.id, [update.message.id])
+                    deleted = True
+        if deleted == False:
+            result = await bot(functions.users.GetFullUserRequest(id=update.message.from_id))
+            if(result.user.bot or any(list(map(result.about.__contains__, ['بیوگرافیمو بخون', 'relexton', 'بیا پیوی', 'سکس حضوری', 'سسکی', 'صصکی', 'محصولاتم'])))):
+                user = await bot(functions.channels.GetParticipantRequest(channel=update.message.chat.id, user_id=update.message.from_id))
+                rights = user.participant.__class__.__name__
+                if rights != ' ChannelParticipantCreator' and rights != 'ChannelParticipantAdmin':
+                    await bot.delete_messages(update.message.chat.id, [update.message.id])
+
+bot.start()
+bot.run_until_disconnected()
